782d2f3e30c566769149194f7f66a7ff780b0697
Aidan Ridderhof <aidan.ridderhof@gmail.com>
2024-07-10 17:44:54 -0400

    Create getComputerChoice function

---
+++ javascript.js
@@ -0,0 +1,19 @@
+console.log("hello world")
+
+
+function getComputerChoice() {
+    let x = Math.random();
+    if (x<.34) {
+        return "Rock"
+    }
+
+    else if (x<.67) {
+        return "Paper"
+    }
+
+    else {
+        return "Scissors"
+    }
+}
+
+console.log(getComputerChoice())
